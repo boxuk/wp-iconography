e783838b4f6f7a086be57941c2970fa9ad638195
boxuk-wp-robot <developers@boxuk.com>
2024-07-04 07:00:01 +0000

    Merge pull request #35 from boxuk/iconography/configurable-icon-sets

---
+++ build/block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'd477bda2be1cd546289f');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '2b50a1b034ba1bd23a01');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '6883a3301333b5c4fdbf');
