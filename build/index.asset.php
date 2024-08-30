95e3b298fb4ac77bdc7912fcca151037d7162f41
boxuk-wp-robot <developers@boxuk.com>
2024-08-30 05:09:23 +0000

    Merge pull request #118 from boxuk/dependabot/npm_and_yarn/npm_and_yarn-7aabbe6aff

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '6883a3301333b5c4fdbf');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '14246a3c7cb6604a0cff');
